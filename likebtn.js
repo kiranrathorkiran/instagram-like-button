4e23da6699e079bc22f2253d071420ded7cec022
kiran <kiranrathor8085@gmail.com>
2023-12-22 19:02:39 +0530

    first commit

---
+++ likebtn.js
@@ -0,0 +1,20 @@
+
+
+//console.log("good morning")
+var con = document.querySelector("#cantaner")
+var love = document.querySelector("i")
+con.addEventListener("dblclick",function()
+{    
+    love.style.transform='translate(-50%,-50%) scale(1)'
+    love.style.opacity=0.8
+
+    setTimeout(function () {
+      love.style.opacity=0
+       
+    },2000)
+    setTimeout(function () {
+        love.style.transform='translate(-50%,-50%) scale(0)'
+         
+      },3000)
+})
+
